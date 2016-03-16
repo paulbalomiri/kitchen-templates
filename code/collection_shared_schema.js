296dfe69fda51ba1fab039c6a3754927842b9665
created a lodash deep meteor package <paulbalomiri@gmail.com>
2016-03-16 11:49:43 +0200

    dirty hack to be reverted when the issue is resolved

--- code/collection_shared_schema.js
+++ code/collection_shared_schema.js
@@ -1,5 +1,7 @@
 this.Schemas = this.Schemas || {};
 
-this.Schemas.COLLECTION_VAR = new SimpleSchema(SIMPLE_SCHEMA);
-
+var objKeysRegex = /({|,)(?:\s*)(?:')?([A-Za-z_$\.][A-Za-z0-9_ \-\.$]*)(?:')?(?:\s*):/g;
+var json = `SIMPLE_SCHEMA`.replace(objKeysRegex, "$1\"\$2\":");		    
+eval( "json="+json);
+this.Schemas.COLLECTION_VAR = new SimpleSchema(json);
 this.COLLECTION_VAR.attachSchema(this.Schemas.COLLECTION_VAR);
